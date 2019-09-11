a42472fd25223c3c0afc328abbe8107ce0359413
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-11 12:42:55 +0600

    button in action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>First App Wokrd</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Welcome to the Home Screen</Text>
+      {/* Button only takes values as props */}
+      <Button
+        title="Go to components demo"
+        onPress={() => console.log("Button is pressed")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,10 +3,15 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend#1" },
-    { name: "Friend#2" },
-    { name: "Friend#3" },
-    { name: "Friend#4" }
+    { name: "Friend#1", age: 20 },
+    { name: "Friend#2", age: 20 },
+    { name: "Friend#3", age: 20 },
+    { name: "Friend#4", age: 20 },
+    { name: "Friend#5", age: 20 },
+    { name: "Friend#6", age: 20 },
+    { name: "Friend#7", age: 20 },
+    { name: "Friend#8", age: 20 },
+    { name: "Friend#9", age: 20 }
   ];
 
   return (
@@ -15,16 +20,27 @@ const ListScreen = () => {
     //friend arg is each friend object
     //inside keyExtractor inspect each obj and return unique key for each one
     <FlatList
+      //   horizontal // horizontal listing
+      //   showsHorizontalScrollIndicator={false}
       keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({ item }) => {
         //element.item holds the individual item
-        return <Text>{item.name}</Text>;
+        return (
+          <Text style={styles.testStyle}>
+            {item.name}-{item.age}
+          </Text>
+        );
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  testStyle: {
+    //adding margin to above & below each list item
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;
