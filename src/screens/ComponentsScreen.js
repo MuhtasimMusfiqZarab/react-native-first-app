9dbc345569dcf6f4466a1f4739cc32f962ab2295
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-11 10:30:29 +0600

    initial commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ComponentScreen = () => {
+  return <Text style={styles.textStyle}>Components Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30
+  }
+});
+
+export default ComponentsScreen;
