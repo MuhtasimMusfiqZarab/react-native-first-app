67c980d01ec413ca0fed076c55d33e4a1c3de2ab
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-12 11:48:20 +0600

    applying state with a reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,54 +1,65 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text } from "react-native";
 //reusable component
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_CHANGE = 15;
+
+//write reducer before usereducer call (outside the component for avaoiding confusion)
+//reducers always returns something
+// reducer is called with two objects (state obj, obejct define how to change state)
+//action =>> how we change state object
+const reducer = (state, action) => {
+  //state==={red :number, green: number, blue: number}
+  //action=={colorTOChange: red || green || blue, amount:15 || -15}
+  //check action first to make changes
+  switch (action.colorToChange) {
+    case "red":
+      // returning new state without changing the preivous one
+      return { ...state, red: state.red + action.amount };
+    case "green":
+      return { ...state, green: state.green + action.amount };
+    case "blue":
+      return { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
+
 const SquareScreen = () => {
-  //here are the states
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-
-  const COLOR_CHANGE = 15;
-
-  // this will change the color states accordingly (validate changes)
-  const setColor = (color, change) => {
-    switch (color) {
-      case "red":
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        break;
-      case "green":
-        green + change > 255 || green + change < 0
-          ? null
-          : setGreen(green + change);
-        break;
-      case "blue":
-        blue + change > 255 || blue + change < 0
-          ? null
-          : setBlue(blue + change);
-        break;
-
-      default:
-        break;
-    }
-  };
+  //dispatch(runReducer)
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  //destructuring state
+  const { red, green, blue } = state;
 
   return (
     <View>
       {/* These three will change the rgb values in the state */}
       <ColorCounter
-        onIncrease={() => setColor("red", COLOR_CHANGE)}
-        onDecrease={() => setColor("red", -1 * COLOR_CHANGE)}
+        onIncrease={() =>
+          dispatch({ colorToChange: "red", amount: COLOR_CHANGE })
+        } //dispatching action obj(action obj defines how to change state)
+        onDecrease={() =>
+          dispatch({ colorToChange: "red", amount: -1 * COLOR_CHANGE })
+        }
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setColor("green", COLOR_CHANGE)}
-        onDecrease={() => setColor("green", -1 * COLOR_CHANGE)}
+        onIncrease={() =>
+          dispatch({ colorToChange: "blue", amount: COLOR_CHANGE })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "blue", amount: -1 * COLOR_CHANGE })
+        }
         color="Blue"
       />
       <ColorCounter
-        onIncrease={() => setColor("blue", COLOR_CHANGE)}
-        onDecrease={() => setColor("blue", -1 * COLOR_CHANGE)}
+        onIncrease={() =>
+          dispatch({ colorToChange: "green", amount: COLOR_CHANGE })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: "green", amount: -1 * COLOR_CHANGE })
+        }
         color="Green"
       />
 
